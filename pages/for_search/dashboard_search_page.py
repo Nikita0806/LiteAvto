503513469c4daa89b55d5e10d77728cdfddc5ee0
Nikita0806 <volkov_n@garpix.com>
2024-02-09 12:26:30 +0300

    first commit

--- pages/for_search/dashboard_search_page.py
+++ pages/for_search/dashboard_search_page.py
@@ -18,38 +18,56 @@ class SearchMarka(BasePage):                                      # локато
     SELECT_CUZOV = ("xpath", "/html/body/div[2]/form/p[4]/select/option[3]")     # Седан
     EXPLORE_BUTTON = ("xpath", "/html/body/div[2]/form/button")                  # Искать
     CAR_CARD = ("xpath", "/html/body/div[2]/ul/li/a")                            # Карточка авто
-
-    # DATE_ENTRY_FROM = ("xpath", "/html/body/div[2]/form/p[5]/input']")
+    START_PRICE = ("xpath", "/html/body/div[2]/form/p[8]/input")                 # Цена от
+    END_PRICE = ("xpath", "/html/body/div[2]/form/p[7]/input")                   # Цена до
+    START_MILEAGE = ("xpath", "/html/body/div[2]/form/p[9]/input")               # Пробег от
+    END_MILEAGE = ("xpath", "/html/body/div[2]/form/p[10]/input")                # Пробег до
 
 
     # @allure.step("Click submit button")                         #для алюра
     def click_search_marka(self):                              # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SEARCH_MARKA)).click()
-        time.sleep(1)
+        # time.sleep(1)
 
     # @allure.step("Click submit button")                         #для алюра
     def click_select_marka(self):                              # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SELECT_MARKA)).click()
-        time.sleep(1)
+        # time.sleep(1)
 
     # @allure.step("Click submit button")                         #для алюра
     def click_search_model(self):                              # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SEARCH_MODEL)).click()
-        time.sleep(1)
+        # time.sleep(1)
 
     # @allure.step("Click submit button")                         #для алюра
     def click_select_model(self):                              # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SELECT_MODEL)).click()
-        time.sleep(1)
+        # time.sleep(1)
 
     # @allure.step("Click submit button")                         #для алюра
     def click_search_cuzov(self):  # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SEARCH_CUZOV)).click()
-        time.sleep(1)
+        # time.sleep(1)
 
         # @allure.step("Click submit button")                         #для алюра
     def click_select_cuzov(self):  # нажимает на кнопку
         self.wait.until(EC.element_to_be_clickable(self.SELECT_CUZOV)).click()
+        # time.sleep(1)
+
+    def start_price(self, new_price):  # для смены текста в поле
+        self.wait.until(EC.element_to_be_clickable(self.START_PRICE)).send_keys(new_price)  #
+        time.sleep(1)
+
+    def end_price(self, new_price):  # для смены текста в поле
+        self.wait.until(EC.element_to_be_clickable(self.END_PRICE)).send_keys(new_price)  #
+        time.sleep(1)
+
+    def start_mileage(self, new_mileage):  # для смены текста в поле
+        self.wait.until(EC.element_to_be_clickable(self.START_MILEAGE)).send_keys(new_mileage)  #
+        time.sleep(1)
+
+    def end_mileage(self, new_mileage):  # для смены текста в поле
+        self.wait.until(EC.element_to_be_clickable(self.END_MILEAGE)).send_keys(new_mileage)  #
         time.sleep(1)
 
         # @allure.step("Click submit button")                         #для алюра
@@ -63,11 +81,3 @@ class SearchMarka(BasePage):                                      # локато
         time.sleep(5)
 
 
-
-
-
-# ввод текста
-    # def year_from(self, new_date):  # для смены текста в поле
-    #     self.wait.until(EC.element_to_be_clickable(self.DATE_ENTRY_FROM)).send_keys(new_date)  #
-    #
-    #     time.sleep(2)
